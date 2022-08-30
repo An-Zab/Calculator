ce891c7ab820bfafa4bd61b71055820cca2d88d4
unknown <tolyan4ik27111998@gmail.com>
2022-08-30 19:39:10 +0400

    intermidiate coomit. Improving UI, modifying styles and color palette expansion

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    lateinit var workingTV : TextView
+    lateinit var resultTV : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun backspacefunction(view: View) {}
+    fun clearallfunction(view: View) {
+        workingTV = findViewById(R.id.workingTV)
+        workingTV.text = ""
+        resultTV = findViewById(R.id.resultTV)
+        resultTV.text = ""
+    }
+    fun equalsfunction(view: View) {}
 }

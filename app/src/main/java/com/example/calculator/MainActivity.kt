1af72e5a5d892f6842d0cd212b69d1bcd22c153e
unknown <tolyan4ik27111998@gmail.com>
2022-09-01 17:13:17 +0400

    Create a structure to represent the numbers entered.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var workingTV : TextView
-    lateinit var resultTV : TextView
+    private var strNumber = StringBuilder()
+    private lateinit var workingTV: TextView
+    private lateinit var resultTV: TextView
+    private lateinit var numberButtons : Array<Button>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        workingTV = findViewById(R.id.workingTV)
+        resultTV = findViewById(R.id.resultTV)
+        numberButtons = arrayOf(id0, id1, id2, id3, id4, id5, id6, id7, id8, id9)
+        for (i in numberButtons) { i.setOnClickListener { numberButtonclick(i) } }
+
+
     }
 
-    fun backspacefunction(view: View) {}
-    fun clearallfunction(view: View) {
-        workingTV = findViewById(R.id.workingTV)
-        workingTV.text = ""
-        resultTV = findViewById(R.id.resultTV)
-        resultTV.text = ""
+
+    private fun numberButtonclick(btn : Button) {
+        strNumber.append(btn.text)
+        workingTV.text = strNumber
     }
-    fun equalsfunction(view: View) {}
 }
+

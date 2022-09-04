8210ab6267069f40fec80f0b99e5ea470e33a53c
unknown <tolyan4ik27111998@gmail.com>
2022-09-04 17:04:23 +0400

    Changes in the principle of the AC, equal, brackets buttons. Adding exceptions.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.mariuszgromada.math.mxparser.*
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +22,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var e = Expression("2^3+2^(-3)+2^3^(-4)")
-        Log.d("MyLog", "Res: " + e.getExpressionString() + " = " + e.calculate())
         workingTV = findViewById(R.id.workingTV)
         resultTV = findViewById(R.id.resultTV)
+
         numberButtons = arrayOf(id0, id1, id2, id3, id4, id5, id6, id7, id8, id9)
         for (i in numberButtons) { i.setOnClickListener { numberButtonclick(i) } }
+
         actionButtons = arrayOf(idplus, idmultiply, idminus, iddivide, idbrackets)
         for (i in actionButtons) { i.setOnClickListener { actionButtonclick(i) } }
         idbrackets.setOnClickListener {
@@ -36,13 +37,47 @@ class MainActivity : AppCompatActivity() {
             }
             else  {strNumber.append(idbrackets.text[3])
             workingTV.text = strNumber}
-            a=a+1
+            a++
         }
         AC.setOnClickListener {
             strNumber.delete(0,strNumber.length)
+            resultTV.text = ""
             workingTV.text = strNumber
+            a = 1
+        }
+        iddelete.setOnClickListener{
+            try {
+                strNumber.setLength(strNumber.length-1)
+                workingTV.text = strNumber
+            } catch (e: Exception){
+                workingTV.text = ""
+            }
+        }
+        idequal.setOnClickListener {
+            if(strNumber.length >= 1) {
+                val e = Expression(strNumber.toString())
+                resultTV.text = e.calculate().toString()
+                strNumber.delete(0, strNumber.length) }
+            else {
+                strNumber.append(resultTV.text)
+                workingTV.text = strNumber
+                resultTV.text = ""
+            }
+        }
+        idpercents.setOnClickListener {
+            if (strNumber.length>=1) {
+                val e = Expression(strNumber.toString())
+                var a = e.calculate()/100
+                strNumber.delete(0, strNumber.length)
+                strNumber.append(a.toString())
+                resultTV.text = "$a"
+            } else {
+                strNumber.delete(0, strNumber.length)
+                resultTV.text = "Format error"
+            }
         }
     }
+
      private fun actionButtonclick(btn: Button) {
         strNumber.append(btn.text)
         workingTV.text = strNumber
@@ -51,7 +86,6 @@ class MainActivity : AppCompatActivity() {
         strNumber.append(btn.text)
         workingTV.text = strNumber
     }
-
 
 }
 
